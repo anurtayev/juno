e99595d5e6a3b800b6075ad6bf2edad39e29b064
all <all@alls-MacBook-Air.local>
2016-01-17 17:49:45 -0500

    .

--- packages/ts-etnry/package.js
+++ packages/ts-etnry/package.js
@@ -13,7 +13,8 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.addFiles('ts-etnry.js');
+  api.use('react');
+  api.addFiles('timesheet-entry.jsx');
   api.export('TimesheetEntry', 'client');
 });
 
