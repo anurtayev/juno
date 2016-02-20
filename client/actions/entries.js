3fda394c694fbf4657ca7ff20e1f19a47257e9bb
Adil Nurtayev <anurtayev@yahoo.com>
2016-02-20 18:15:05 +0000

    floating number of hours

--- client/actions/entries.js
+++ client/actions/entries.js
@@ -1,13 +1,20 @@
 export default {
   insert({Meteor, LocalState, FlowRouter}, entry) {
-    
+
     if (
-        !entry.date || 
+        !entry.date ||
         !entry.projectNumber ||
         !entry.projectName ||
         !entry.projectTask ||
-        !entry.hours 
+        !entry.hours
     ) return LocalState.set('SAVING_ERROR', 'required values are missing...');
+
+    const hours = parseFloat(entry.hours);
+    if (hours) {
+      entry.hours = hours;
+    } else {
+      return LocalState.set('SAVING_ERROR', 'value of hours is incorrect...');
+    }
 
     if (entry._id) {
       // update
@@ -33,30 +40,30 @@ export default {
   clearErrors({LocalState}) {
     return LocalState.set('SAVING_ERROR', null);
   },
-  
+
   submit({Meteor, FlowRouter}) {
     if (confirm('This will SUBMIT entries for invoicing!')) {
       Meteor.call('entries.submit');
       FlowRouter.go(`/engineering`);
     }
   },
-  
+
   navigateNewEntry({FlowRouter}) {
     FlowRouter.go('/newentry');
   },
-  
+
   navigateEngineering({FlowRouter}) {
     FlowRouter.go('/engineering');
   },
-  
+
   deleteEntry({Meteor}, entryId) {
     Meteor.call('entries.delete', entryId);
   },
-  
+
   editEntry({Meteor, FlowRouter}, entryId) {
     FlowRouter.go(`/edit/${entryId}`);
   },
-  
+
   copyEntry({Meteor, Collections, LocalState, FlowRouter}, entryId) {
     const entry = Collections.Entries.findOne({ _id: entryId });
     entry._id = Meteor.uuid();
@@ -67,7 +74,7 @@ export default {
       }
     });
   },
-  
+
   sortByOnClick({FlowRouter}, sortBy) {
     FlowRouter.go(`/engineering?sortBy=${ sortBy === 'date' ? 'projectNumber' : 'date' }`);
   }
--- client/components/edit/index.jsx
+++ client/components/edit/index.jsx
@@ -12,42 +12,42 @@ import TaskInput from '../taskInput/index.jsx';
 import ToolBar from './tb.jsx';
 
 export default class Edit extends React.Component {
-  
+
   constructor(props) {
     super(props);
-    
+
     this.state = {
       _id: props.entry ? props.entry._id : null,
       date: props.entry ? props.entry.date : null,
-      hours: props.entry ? props.entry.hours : 0,
+      hours: props.entry ? props.entry.hours : null,
       description: props.entry ? props.entry.description : '',
       submitted: props.entry ? props.entry.submitted : false,
       invoiced: props.entry ? props.entry.invoiced : false,
       createdAt: props.entry ? props.entry.createdAt : new Date(),
       userId: props.entry ? props.entry.userId : this.props.userId,
       username: props.entry ? props.entry.username : this.props.username,
-      
+
       project: props.entry ? props.projects.find( (element, index, array) => { return props.entry.projectNumber === element.projectNumber ? true : false } ) : null,
-      
+
       projectTask: props.entry ? props.entry.projectTask : '',
       taskInput_value: props.entry ? props.entry.projectTask : ''
     };
   }
-  
+
   render() {
     const {error} = this.props;
-    
+
     const underlineStyle = {
       display: 'none'
     };
-    
+
     const style = {
       marginLeft: 20
     };
-    
+
     return (
       <div>
-      
+
         <ToolBar
           cancelAction={this.props.navigateEngineering}
           saveAction={this.props.saveEntry.bind(this, {
@@ -65,85 +65,85 @@ export default class Edit extends React.Component {
             username: this.state.username,
           })}
         />
-          
+
         {error ? <p style={{color: 'red'}}>{error}</p> : null}
         <Paper zDepth={2}>
-        
-          <DatePicker 
-            hintText='Date' 
-            autoOk={true} 
-            value={this.state.date} 
-            onChange={this.dateOnChange.bind(this)} 
-            container='inline' 
-            style={style} 
+
+          <DatePicker
+            hintText='Date'
+            autoOk={true}
+            value={this.state.date}
+            onChange={this.dateOnChange.bind(this)}
+            container='inline'
+            style={style}
             underlineStyle={underlineStyle}
           />
-          
+
           <Divider />
-          
-          <ProjectInput 
-            underlineStyle={underlineStyle} 
-            style={style} 
-            projectOnChange={this.projectOnChange.bind(this)} 
+
+          <ProjectInput
+            underlineStyle={underlineStyle}
+            style={style}
+            projectOnChange={this.projectOnChange.bind(this)}
             projects={this.props.projects}
             selectedProject={this.state.project}
           />
-          
+
           <Divider/>
-          
-          <TaskInput 
-            tasks={this.state.project ? this.state.project.tasks : []} 
-            underlineStyle={underlineStyle} 
-            style={style} 
+
+          <TaskInput
+            tasks={this.state.project ? this.state.project.tasks : []}
+            underlineStyle={underlineStyle}
+            style={style}
             taskOnChange={this.taskOnChange.bind(this)}
             taskOnSelect={this.taskOnSelect.bind(this)}
             value={this.state.taskInput_value}
           />
-          
+
           <Divider />
-          
-          <TextField 
-            hintText='Hours' 
-            underlineStyle={underlineStyle} 
-            style={style} 
-            onChange={this.hoursOnChange.bind(this)} 
+
+          <TextField
+            hintText='Hours'
+            underlineStyle={underlineStyle}
+            style={style}
+            onChange={this.hoursOnChange.bind(this)}
             value={this.state.hours}
           />
-          
+
           <Divider />
-          
-          <TextField 
-            hintText='Description' 
-            underlineStyle={underlineStyle} 
-            style={style} 
-            onChange={this.descriptionOnChange.bind(this)} 
-            fullWidth 
+
+          <TextField
+            hintText='Description'
+            underlineStyle={underlineStyle}
+            style={style}
+            onChange={this.descriptionOnChange.bind(this)}
+            fullWidth
             value={this.state.description}
           />
-          
+
           <Divider />
         </Paper>
       </div>
     )
   }
-  
+
   projectOnChange(project) {
     this.setState({project: project});
     this.taskOnSelect('');
   }
-  
+
   dateOnChange(event, date) {
     this.setState({date: date});
   }
-  
+
   hoursOnChange(event) {
-    this.setState({hours: isNaN(parseInt(event.target.value)) ? 0 : parseInt(event.target.value)});
+    this.setState({hours: event.target.value});
   }
-  
+
   descriptionOnChange(event) {
     this.setState({description: event.target.value});
   }
-  
+
   taskOnChange(e) {
     if (e) this.setState({ taskInput_value: e.target.value });
   }
